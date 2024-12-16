1b03fb33cbad052820a22a3aa33e53a7ce10a631
devcodecampos <devcodecampos@gmail.com>
2024-12-16 16:33:30 -0300

    chore: configuração inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_read_root_should_return_ok_and_helloworld():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Hello World'}  # Assert
